3b89f3ba15ea0e25b518ea9ff407ad473dacdd82
Rashid S <rashids24680@gmail.com>
2024-06-12 10:17:38 +0530

    next Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
         <motion.div
             whileInView={{opacity: 1, x: 0}}
             initial={{opacity: 0, x: -100}}
-            transition={{duration: 0.5, delay: 1}}>
+            transition={{duration: 0.5, delay: 0.5}}>
             <a href="#" className="border-b">{contact.email}</a>
         </motion.div>
         </div>
